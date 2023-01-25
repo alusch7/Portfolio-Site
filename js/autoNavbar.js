8beb4ca64588070b0cf25545a333b1caefa952a7
alusch7 <luschera@mcmaster.ca>
2023-01-25 18:39:19 -0500

    Added some CSS and JS for navbar animations

---
+++ js/autoNavbar.js
@@ -0,0 +1,19 @@
+var prevScrollPosition = window.scrollY;
+
+window.onscroll = function() {
+    var bar = document.getElementById("mainNav");
+    const height = bar.offsetHeight;
+    var currentScrollPosition = window.scrollY;
+    console.log(height);
+    if (currentScrollPosition < height-20) {
+        bar.classList.add("atTop");
+    } else {
+        bar.classList.remove("atTop");
+    }
+    if (prevScrollPosition > currentScrollPosition) {
+        bar.style.top = "0";
+    } else if (currentScrollPosition >= (height+10)){
+        bar.style.top = "-100%";
+    }
+    prevScrollPosition = currentScrollPosition;
+}
